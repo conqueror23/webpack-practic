2783679a6af4e5c5df79693f7e1d952d887c3fc9
bruce <bruce.wang@zerologix.com>
2020-06-05 21:16:57 +1000

    transform to react based project

---
+++ src/app/components/index.js
@@ -0,0 +1,14 @@
+import HowTo from './how-to-apply/HowToApply'
+
+import SubForm from './subscribe-form/SubscirbeForm'
+
+import TopTrader from './top-traders/TopTraders'
+
+
+export const {
+
+    HowTo,
+    SubForm,
+    TopTrader
+
+}
