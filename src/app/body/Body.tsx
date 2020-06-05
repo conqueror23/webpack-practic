e4ae4b660c5fa1e11348c7b2e14b416090bab04d
bruce.wang <bruce.wang@zerologix.com>
2020-06-05 19:39:31 +1000

    update webpack test proejct f

---
+++ src/app/body/Body.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Body = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Body
