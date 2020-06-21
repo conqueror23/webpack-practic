a2a542935874f0e18169731bde727dc7e8f0c84f
wayneborong@gmail.com <wayneborong@gmail.com>
2020-06-21 16:24:10 +1000

    update windows local

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 const devSeverOptions={
   devServer:{
-    contentBase:path.join(__dirname,'dist'),
+    contentBase:path.join(__dirname,'./src'),
+    hot:true,
     compress:true,
     port:8000,
   }
@@ -30,20 +31,31 @@ const moduleOptions={
 //   filename: `${packages}-[contentHash:5].js`,
 //   path: path.resolve(__dirname, `dist/${packages}`),
 // }
+
 const commonSetting=(packages)=>{return {
-  entry: path.resolve(__dirname,'src',packages,'index.js'),
+  entry: path.resolve(packages,'index.js'),
+  shared: ['react', 'react-dom'],
   module: {...moduleOptions},
+  output:{
+    libraryTarget:"umd", 
+    filename:"bundle.js",
+  },
+  stats:{ colors: true},
 }
 }
 module.exports = () => {
   const [operations,packages] = process.env.npm_lifecycle_event.split(":");
+  // console.log('path here',path.resolve(__dirname,'./src',packages,'index.js'));
   // settings for static generating renamed css and js with random numbers
   // console.log(`operations ${operations} and the packages ${packages}`);
 
   if(operations.indexOf('dev')>-1){
     return config={
       ...commonSetting,
-      // ...devSeverOptions
+      // resolve: {
+      //   modules: ['node_modules','src']
+      // },
+      ...devSeverOptions
     }
   }else{
     return config={...commonSetting}
